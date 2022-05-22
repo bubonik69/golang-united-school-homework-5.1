9ff1fc23f9b8f52af45390f63fa7a1d32b987fce
Artem <ruteboy69@gmail.com>
2022-05-22 19:53:14 +0300

    2

--- square.go
+++ square.go
@@ -9,14 +9,17 @@ type Square struct {
 	a     uint
 }
 
-func (receiver) End() Point {
+func (s Square) End() Point {
 	// implement me
+	return Point{s.start.x + int(s.a), s.start.y-int(s.a)}
 }
 
-func (receiver) Area() uint {
+func (s Square) Area() uint {
 	// implement me
+	return s.a*s.a
 }
 
-func (receiver) Perimeter() uint {
+func (s Square) Perimeter() uint {
 	// implement me
+	return s.a*4
 }
